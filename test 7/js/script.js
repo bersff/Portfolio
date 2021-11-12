f8492002165efdb1b8407b4dcb713e83903f111f
bersff <74816054+bersff@users.noreply.github.com>
2021-11-12 10:41:55 +0300

    Test site 7
    Without bootstrap

---
+++ test 7/js/script.js
@@ -0,0 +1,46 @@
+/*burger*/
+
+function onClickMenu(){
+    document.getElementById("nav-burger").classList.toggle("active");
+    document.getElementById("body-right").classList.toggle("active");
+}
+
+/*-------------------------------------------------------------------------------*/
+
+/*plus cards*/
+
+function onClickMenuMore(){
+    document.getElementById("visually-hidden").classList.toggle("active");
+    document.getElementById("body-right").classList.toggle("active");
+}
+
+/*-------------------------------------------------------------------------------*/
+
+/*chevron*/
+
+function onClickDistance(){
+    document.getElementById("distance-title").classList.toggle("active");
+    document.getElementById("distance-chevron").classList.toggle("active");
+    document.getElementById("distance-chevron1").classList.toggle("active");
+    document.getElementById("chevronUp").classList.toggle("active");
+}
+function onClickTime(){
+    document.getElementById("time-title").classList.toggle("active");
+    document.getElementById("time-chevron").classList.toggle("active");
+    document.getElementById("time-chevron1").classList.toggle("active");
+    document.getElementById("time-chevron2").classList.toggle("active");
+    document.getElementById("time-chevron3").classList.toggle("active");
+    document.getElementById("chevronUp2").classList.toggle("active");
+}
+function onClickSettings(){
+    document.getElementById("settings-title").classList.toggle("active");
+    document.getElementById("settings-chevron").classList.toggle("active");
+    document.getElementById("settings-chevron1").classList.toggle("active");
+    document.getElementById("settings-chevron2").classList.toggle("active");
+    document.getElementById("settings-chevron3").classList.toggle("active");
+    document.getElementById("settings-chevron4").classList.toggle("active");
+    document.getElementById("settings-chevron5").classList.toggle("active");
+    document.getElementById("chevronUp3").classList.toggle("active");
+}
+
+/*-------------------------------------------------------------------------------*/
