f49bc8901be298fb479d1a375b24d279b678f796
bersff <74816054+bersff@users.noreply.github.com>
2022-02-05 10:44:23 +0300

    Cherry

---
+++ CHERRY/js/script.js
@@ -0,0 +1,10 @@
+
+if (window.matchMedia("(max-width: 380px)").matches) {
+	let search = document.querySelector('.starting_input');
+	let search1 = document.querySelector('.starting_btn');
+  	search.remove();
+  	search1.remove();
+ }
+ else {
+      console.log(stop)
+  }
